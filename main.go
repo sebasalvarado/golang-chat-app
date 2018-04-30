8cc35a7b4f73588fe40113af180fc198ab3f3a21
Sebastian Alvarado <Nixon.Alvarado@ibm.com>
2018-04-30 10:08:35 -0400

    Receiving the addres as cli param

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"path/filepath"
@@ -19,10 +20,13 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
+	// Parse the cli addres
+	var addr = flag.String("addr", ":8080", "The port where the app will listen to")
+	flag.Parse()
 	// Create a new chat room
 	r := newRoom()
 	// Root path
@@ -32,7 +36,8 @@ func main() {
 	// get the room going
 	go r.run()
 	// start web server
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("Starting a web server on", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe", err)
 	}
 }
